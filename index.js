7fe71c85a5e57685f998718cb3bb663e5095acee
mlmirk <mike.l.mirkovic@gmail.com>
2022-04-29 10:07:00 -0700

    add port info

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const server = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log("server Listening....");
